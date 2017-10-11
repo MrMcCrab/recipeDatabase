95cab2066f89b0ff77ec62195e8e572d539fdac4
MrMcCrab <>
2017-10-11 22:53:57 +0300

    Index

---
+++ index.php
@@ -0,0 +1,46 @@
+
+<?php
+  include 'header.php';
+ ?>
+
+  <body>
+    <div class="container">
+
+      <div class="row">
+        <div class="col-md-4">
+
+        </div>
+        <div class="col-md-4">
+          <div class="jumbotron">
+
+          </div>
+        </div>
+        <div class="col-md-4">
+
+        </div>
+      </div>
+
+      <div class="row">
+
+        <div class="col-md-4">
+
+        </div>
+
+        <div class="col-md-4">
+          <a href="addRecipe.php" class="btn btn-success">Lisää resepti</a>
+          <a href="searchRecipes.php" class="btn btn-success">Etsi reseptejä</a>
+        </div>
+
+        <div class="col-md-4">
+
+        </div>
+
+      </div>
+    </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
